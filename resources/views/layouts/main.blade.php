2f6af2827b18e63e44cbe9d28f482d52d3cf5ca9
nierma281 <nierma281@gmail.com>
2025-11-15 08:15:59 +0800

    update main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,16 +10,25 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
   <div class="container">
+    
     <a class="navbar-brand" href="/">Home</a>
-    <div class="collapse navbar-collapse">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-3">
         <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
         <li class="nav-item"><a class="nav-link" href="#">2301010127 ZahwaAuliaPutri</a></li>
         <li class="nav-item"><a class="nav-link" href="#">2301010103 Nirmayana</a></li>
       </ul>
     </div>
+
   </div>
 </nav>
+
 
 <main class="container my-4">
   @yield('content')
